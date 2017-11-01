e775d11a415dd6b590572def6f666d0b64cd19af
Steven Li <listeven@usc.edu>
2017-10-31 18:40:10 -0700

    only process input if has authority

--- Assets/PlayerCharacter.cs
+++ Assets/PlayerCharacter.cs
@@ -15,23 +15,26 @@ public class PlayerCharacter : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKey(KeyCode.A))
+        if (hasAuthority)
         {
-            body.AddForce(Vector3.left * movementForce);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            body.AddForce(Vector3.forward * movementForce);
+            if (Input.GetKey(KeyCode.A))
+            {
+                body.AddForce(Vector3.left * movementForce);
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                body.AddForce(Vector3.forward * movementForce);
 
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            body.AddForce(Vector3.left * -movementForce);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                body.AddForce(Vector3.left * -movementForce);
 
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            body.AddForce(Vector3.forward * -movementForce);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                body.AddForce(Vector3.forward * -movementForce);
+            }
         }
     }
 }
